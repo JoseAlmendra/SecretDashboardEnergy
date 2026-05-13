7e19a67d32c228466f8d1bf3bd2dedb3016487dd
JoseAlmendra <54294829+JoseAlmendra@users.noreply.github.com>
2026-05-13 20:31:06 +0000

    logsss

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const { Pool } = require('pg');
+const path = require('path');
 
 const app = express();
 
@@ -22,12 +23,32 @@ pool.connect((err, client, release) => {
 });
 console.log(process.env.DATABASE_URL);
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('Servidor funcionando');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+// 4. Ruta de Login
+app.post('/login', async (req, res) => {
+  const { nombre, password } = req.body;
+  try {
+    const query = 'SELECT * FROM usuarios WHERE nombre = $1 AND password = $2';
+    const result = await pool.query(query, [nombre, password]);
 
-const PORT = process.env.PORT || 8080;
+    if (result.rows.length > 0) {
+      res.json({ 
+        mensaje: "Bienvenido", 
+        usuario: result.rows[0].nombre,
+        rol: result.rows[0].rol 
+      });
+    } else {
+      res.status(401).json({ mensaje: "Credenciales incorrectas" });
+    }
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).json({ mensaje: "Error en el servidor" });
+  }
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor en puerto ${PORT}`);
