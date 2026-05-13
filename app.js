1b5fcc5e62cf5330d7db29d3ad0c87e62639af4f
JoseAlmendra <54294829+JoseAlmendra@users.noreply.github.com>
2026-05-13 21:17:50 +0000

    react

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ pool.connect((err) => {
 // frontend
 app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
-app.get("/*", (req, res) => {
+// fallback correcto
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
 });
 
