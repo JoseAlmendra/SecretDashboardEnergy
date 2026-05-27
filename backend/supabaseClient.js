d0e98e8d725346e1cac5564ede558fb23c45c4b5
JoseAlmendra <190300398@ucaribe.edu.mx>
2026-05-27 01:42:29 -0500

    Merge: Conflictos resueltos dando prioridad a la versión funcional local

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -4,10 +4,10 @@ const { createClient } = require('@supabase/supabase-js');
 const supabaseUrl = 'https://mhzuhpuhymhiklfsehnd.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1oenVocHVoeW1oaWtsZnNlaG5kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2Mzg2MTMsImV4cCI6MjA5NDIxNDYxM30.zrND5FmYnRPEzJGUr2ELPxc585uaPx23YoAGZPz7zK8';
 
-if (!supabaseUrl || !supabaseKey) {
+if (!supabaseUrl || !supabaseAnonKey) {
   console.error("ERROR: Faltan las variables de entorno de Supabase en el archivo .env");
 }
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 module.exports = supabase;
